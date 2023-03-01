38334b193cdcf81c482139e44cae4600f71dcda7
Ivy Basseches <imb2139@columbia.edu>
2023-03-01 03:49:13 -0500

    Set up blog post (mosaic)

--- _posts/2023-01-28-hello-world.md
+++
@@ -1,9 +0,0 @@
----
-layout: post
-title:  "Hello, world!"
-date:   2023-01-28 19:41:44 -0500
-categories: jekyll project
-permalink: /hello-world
----
-
-My project documentation will be organized as blog posts, like this one.
---
+++ assets/js/copy.js
@@ -0,0 +1,51 @@
+// select all markdown code blocks
+let codeBlocks = document.querySelectorAll('pre.highlight');
+
+codeBlocks.forEach(function(codeBlock) {
+    let copyButton = document.createElement('button');
+    copyButton.className = 'copy';
+    copyButton.type = 'button';
+    copyButton.ariaLabel = 'Copy to clipboard';
+
+    let clipboard = document.createElement('img');
+    clipboard.src = "/3930/assets/static/images/copy.png";
+    clipboard.width = 20;
+
+    copyButton.append(clipboard);
+    codeBlock.append(copyButton);
+
+    copyButton.addEventListener('click', function () {
+
+        // copy text
+        let code = codeBlock.querySelector('code').innerText.trim();
+        window.navigator.clipboard.writeText(code);
+
+        // add checkmark to indicate that the text has been copied
+        copyButton.innerText = '';
+        let check = document.createElement('img');
+        check.src = "/3930/assets/static/images/checkmark.png";
+        check.width = 20;
+        copyButton.append(check);
+
+        let fourSeconds = 4000;
+
+        // after 4 seconds, show the copy to clipboard icon again
+        setTimeout(function () {
+            let clipboard = document.createElement('img');
+            clipboard.src = "/3930/assets/static/images/copy.png";
+            clipboard.width = 20;
+            copyButton.innerText = '';
+            copyButton.append(clipboard);
+        }, fourSeconds);
+
+    });
+
+    // when the user hovers over the code block, show the button to copy
+    codeBlock.addEventListener('mouseover', function () {
+        copyButton.focus({preventScroll:true});
+    });
+    codeBlock.addEventListener('mouseleave', function () {
+        copyButton.blur();
+    });
+
+});
